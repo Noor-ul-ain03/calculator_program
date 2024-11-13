f85be46e036d7a667fa5c7313b41154ccd94fef3
Noor-ul-ain03 <bhattinnoorulain03@gmail.com>
2024-11-13 23:40:38 +0500

    addition freature added

--- calculator.c
+++ calculator.c
@@ -1,11 +1,16 @@
  #include<stdio.h>
 #include<conio.h>
 int main(){
-    int number1,number2,option;
+    int num1,num2,option;
     printf("Enter 2 numbers: ");
-    scanf("%d%d",&number1,&number2);
+    scanf("%d%d",&num1,&num2);
     printf("Press 1:addition \n Press 2:substraction \n Press 3:multiplication \nPress 4: division\n");
     scanf("%d",&option);
-
+ switch(option)
+    {case 1 :
+    printf("addition of %d and %d: %d",num1,num2,num1+num2);
+    break;
   return 0;
-    }
+    } 
+
+  
